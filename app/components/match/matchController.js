58ed7381ad4b835c394569977b1e1b4b63dc4d0a
secondtonone <fixer40@bk.ru>
2015-05-02 02:10:19 +0500

    алгоритм матча с остановкой

--- app/components/match/matchController.js
+++ app/components/match/matchController.js
@@ -1,5 +1,16 @@
 basketballManagerApp.controller('matchController',['$scope','$location','$http','$timeout',function ($scope,$location,$http,$timeout) {
 
+  $scope.period = 1;
+
+  $scope.ownPeriods = [0,0,0,0];
+  $scope.enemyPeriods = [0,0,0,0];
+
+  $scope.scoreOwnTeam = 0;
+  $scope.scoreEnemyTeam = 0;
+
+  $scope.advantage = 50;
+
+  $scope.timeOfMatch = "00 : 00";
 
   $scope.msToString = function (ms)
   {
@@ -7,92 +18,107 @@ basketballManagerApp.controller('matchController',['$scope','$location','$http',
     return (time.getMinutes()<10?'0':'') + time.getMinutes() + " : " + (time.getSeconds()<10?'0':'') + time.getSeconds();
   };
 
-  $scope.period = 1;
 
   $scope.eventsTimer = {
-    i: 0,
-    timers:[],//сгенерированные таймеры всех событий $timeout
-    currentTimer: 0,
-    currentTime: 0,
-    stopTimers: function () {
-      for (var i = currentTimer + 1; i < this.timers.length; ++i) {
+    i: 0,//не знаю за чем
+    timers:[],//сгенерированные таймеры всех событий, промисы от $timeout
+    currentTimer: 0,//порядковый номер таймера
+    currentTime: 0,//время таймера
+    lastTime: 0,
+    stopTimers: function () {//остановка таймеров
+      for (var i = this.currentTimer + 1; i < this.timers.length; ++i) {
         (function(i){
-          $timeout.cancel(timers[i]);
+          $timeout.cancel($scope.eventsTimer.timers[i]);
         })(i);
       }
-      getMatch(period);
+      this.timers = [];
+      this.lastTime = this.currentTime;
+      getEndingMatch($scope.period, this.currentTime);
     },
     makeTimerQueue: function (events) {//генерация таймеров в массив
+      var eventsCount = events.length;
+      var promise = '';
+      for (var i=0; i < eventsCount; ++i) {//цикл по событиям матча
 
-      for (var i=0; i < events.length; ++i) {//пришел 1 тайм
-        this.timers.push(
-          (function (i) {
-            $timeout(function () {
+        (function (i,count) {
+          promise = $timeout(function () {//ангуляр таймер
 
-              $scope.eventsTimer.currentTimer = i;
-              $scope.eventsTimer.currentTime = events.timeEvent*50;
-
-              $scope.scoreOwnTeam = events.score.own;
-              $scope.scoreEnemyTeam = events.score.enemy;
-
-              $scope.ownPeriods[period-1] = events.period.own;
-              $scope.enemyPeriods[period-1] = events.period.enemy;
-
-              $scope.advantage = events.advantage;
-              $scope.timeOfMatch = $scope.msToString(events.timeEvent*50);
-
-              $(".stadium__events__show").prepend('<tr><td class="stadium__events__event">' + events.ownTeam + '</td><td class="stadium__events__time">' + $scope.msToString(events.timeEvent*50) + '</td><td class="stadium__events__event">' + events.enemyTeam + '</td></tr>');
-
-              if (pos == length-1) {
-
-                $(".stadium__events__show").prepend('<tr><td colspan="3" class="stadium__events__end-time">'+ $scope.matchData.period.end + ' ' + period + $scope.matchData.period.th +' '+ $scope.matchData.period.quarter +'</td></tr>');
-
-                $scope.gameStatusNow = $scope.matchData.gameStatus[period];
-              }
-            }, events.timeEvent*50);
-          })(i)
-        );
-        if (i == events.length - 1) {
-          $scope.period += 1;
-        }
+            $scope.eventsTimer.currentTimer = i;
+            $scope.eventsTimer.currentTime = events[i].timeEvent*50;
+            //счет
+            $scope.scoreOwnTeam = events[i].score.own;
+            $scope.scoreEnemyTeam = events[i].score.enemy;
+            //счет периода
+            $scope.ownPeriods[$scope.period-1] = events[i].period.own;
+            $scope.enemyPeriods[$scope.period-1] = events[i].period.enemy;
+            //ползунок и время
+            $scope.advantage = events[i].advantage;
+            $scope.timeOfMatch = $scope.msToString(events[i].timeEvent*50*$scope.period);
+            //вставка события в шаблон
+            $(".stadium__events__show").prepend('<tr><td class="stadium__events__event">' + events[i].ownTeam + '</td><td class="stadium__events__time">' + $scope.msToString(events[i].timeEvent*50) + '</td><td class="stadium__events__event">' + events[i].enemyTeam + '</td></tr>');
+            //если последнее событие
+            if (i == count-1) {
+              //вставка сообщения что период закончен
+              $(".stadium__events__show").prepend('<tr><td colspan="3" class="stadium__events__end-time">'+ $scope.matchData.period.end + ' ' + $scope.period + $scope.matchData.period.th +' '+ $scope.matchData.period.quarter +'</td></tr>');
+              //статус игры
+              $scope.gameStatusNow = $scope.matchData.gameStatus[$scope.period];
+              //следующий период
+              $scope.period += 1;
+              $scope.eventsTimer.timers = [];
+              //вызываем события на след период
+              getMatch($scope.period);
+            }
+          }, events[i].timeEvent*50 - $scope.eventsTimer.lastTime);
+        })(i,eventsCount)
+        //добавляем промисы в массив
+        $scope.eventsTimer.timers.push(promise);
       }
     }
   };
+  //запрос на шаблонные данные
 
   $http.post('/assets/source/matchData.json'/*, {msg:'hello word!'}*/).
   success(function(data, status, headers, config) {
-    // this callback will be called asynchronously
     $scope.matchData = data.matchData;
     $scope.gameStatusNow = $scope.matchData.gameStatus[0];
     $scope.ownTeam = data.matchData.teams.own;
     $scope.enemyTeam = data.matchData.teams.enemy;
-    $scope.ownPeriods = [0,0,0,0];
-    $scope.enemyPeriods = [0,0,0,0];
-
-    $scope.scoreOwnTeam = 0;
-    $scope.scoreEnemyTeam = 0;
-    $scope.advantage = 50;
-    $scope.timeOfMatch = "00 : 00";
   });
-
+  //запрос на период
   function getMatch (period) {
-    $http.post('/assets/source/matchData-' + period + '.json'/*, {msg:'hello word!'}*/).
-      success(function(data, status, headers, config) {
-        // this callback will be called asynchronously
-        var timeOfMatch = 240000;//время матча
-        var i=0;
-        var j=0;
+    //если меньше 5, 5ого периода ведь нет
+    if (period < 5) {
+      $http.post('/assets/source/matchData-' + period + '.json'/*, {msg:'hello word!'}*/).
+        success(function(data, status, headers, config) {
+          $scope.matchEvents = data;
 
-
-        $scope.matchEvents = data;
-
-        $scope.nowOnMatch = new Date().getTime();
-
-        $scope.eventsTimer.makeTimerQueue($scope.matchEvents);
+          $scope.nowOnMatch = new Date().getTime();
+          //делаем очередь из таймеров
+          $scope.eventsTimer.makeTimerQueue($scope.matchEvents);
       });
+    } else {
+      return false;
+    }
   }
+  //запрос на остаток матча
+  function getEndingMatch (period,time) {
+    if (period < 5) {
+      $http.post('/assets/source/matchData-' + period + '-' + time + '.json'/*, {msg:'hello word!'}*/).
+        success(function(data, status, headers, config) {
+          // this callback will be called asynchronously
+          //var timeOfMatch = 240000;//время матча
+          $scope.matchEvents = data;
 
-  getMatch(period);
+          $scope.nowOnMatch = new Date().getTime();
+
+          $scope.eventsTimer.makeTimerQueue($scope.matchEvents);
+      });
+    } else {
+      return false;
+    }
+  }
+  //запрос на первый матч
+  getMatch($scope.period);
 
  /* $http.post('/assets/source/matchData-1.json').
   success(function(data, status, headers, config) {
